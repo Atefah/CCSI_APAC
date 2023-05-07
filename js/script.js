f75c009cac984cc089e54c01132347045135d346
Atefah <atefa.abedi5@gmail.com>
2023-05-07 13:58:37 -0700

    change owerflow-x to owerflow-y in css

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 $(document).ready(function () {
 	$('.invisible-content').hide();
 	$(document).on('click',"#btn", function () {
-		var moreLessButton=$('.invisible-content').is("visible")?'Read More' :'Read Less';
+		var moreLessButton=$('.invisible-content').is(":visible")?'Read More' :'Read Less';
 		$(this).text(moreLessButton);
 		$(this).parent('.box').find('.invisible-content').toggle();
 	 //$(this).parent('.box').find('.visible-content').toggle();
